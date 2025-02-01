9ed6fc4179a9bc60620f4ba1e52b3ef7eea4c689
Beginner <dkstmdwn17@naver.com>
2025-02-01 17:33:03 +0900

    #5 유저 엔티티 설계
    유저 관련 데이터를 효과적으로 관리하기 위해 새로운 유저 엔티티를 설계

---
+++ src/main/java/svsite/matzip/foody/domain/auth/entity/User.java
@@ -0,0 +1,80 @@
+package svsite.matzip.foody.domain.auth.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import java.util.Objects;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import svsite.matzip.foody.global.entity.BaseEntity;
+
+@Entity
+@Table(name = "users", indexes = {
+    @Index(name = "idx_user_email", columnList = "email")
+})
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // 기본 생성자 보호
+public class User extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Enumerated(EnumType.STRING)
+  private LoginType loginType;
+
+  @Column(unique = true, nullable = false, length = 100)
+  private String email;
+
+  @Column(nullable = false, length = 200)
+  private String password;
+
+  @Column(nullable = false, length = 50)
+  private String nickname;
+
+  @Column(length = 255)
+  private String imageUri;
+
+  @Column(length = 255)
+  private String kakaoImageUri;
+
+  @Column(nullable = false, length = 50)
+  private String RED;
+
+  @Column(nullable = false, length = 50)
+  private String YELLOW;
+
+  @Column(nullable = false, length = 50)
+  private String GREEN;
+
+  @Column(nullable = false, length = 50)
+  private String BLUE;
+
+  @Column(nullable = false, length = 50)
+  private String PURPLE;
+
+  @Column(length = 255)
+  private String hashedRefreshToken;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if(!(o instanceof User user)) return false;
+    return id != null && id.equals(user.id);
+  }
+  @Override
+  public int hashCode() {
+    return Objects.hash(getId());
+  }
+}
