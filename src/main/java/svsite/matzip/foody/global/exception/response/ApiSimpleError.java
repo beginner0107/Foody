70e890d2a7686367285197cf622d55c0c43ea738
Beginner <dkstmdwn17@naver.com>
2025-02-02 20:59:49 +0900

    #9 API 응답 객체(ApiResponseError) 및 간단 에러 응답(ApiSimpleError) 정의

---
+++ src/main/java/svsite/matzip/foody/global/exception/response/ApiSimpleError.java
@@ -0,0 +1,38 @@
+package svsite.matzip.foody.global.exception.response;
+
+import java.util.List;
+import lombok.Builder;
+import lombok.NonNull;
+
+@Builder
+public record ApiSimpleError(@NonNull String field, @NonNull String message) {
+  public static List<ApiSimpleError> listOfCauseSimpleError(Throwable cause) {
+    return List.of(normalizeCause(cause));
+  }
+
+  public static ApiSimpleError[] normalizeCause(Throwable cause) {
+    int depth = 0;
+    ApiSimpleError[] subErrors;
+    Throwable currentCause = cause.getCause();
+
+    while (currentCause != null) {
+      currentCause = currentCause.getCause();
+      depth++;
+    }
+
+    subErrors = new ApiSimpleError[depth];
+    currentCause = cause;
+    for (int i = 0; i < depth; i++) {
+      String errorFullName = currentCause.getClass().getSimpleName();
+      String field = errorFullName.substring(errorFullName.lastIndexOf('.') + 1);
+      subErrors[i] = ApiSimpleError.builder()
+          .field(field)
+          .message(currentCause.getLocalizedMessage())
+          .build();
+
+      currentCause = currentCause.getCause();
+    }
+
+    return subErrors;
+  }
+}
