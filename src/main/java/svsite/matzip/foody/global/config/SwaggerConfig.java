fd1936114e6dc1b1324b482a5c3c56d606166e25
Beginner <dkstmdwn17@naver.com>
2025-02-26 23:11:47 +0900

    #25 Swagger https 설정 - https를 등록하기 위해 Server 객체 url 수동 기입

--- src/main/java/svsite/matzip/foody/global/config/SwaggerConfig.java
+++ src/main/java/svsite/matzip/foody/global/config/SwaggerConfig.java
@@ -5,7 +5,9 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import java.util.Arrays;
+import java.util.List;
 import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +23,9 @@ public class SwaggerConfig {
         .version("v0.0.1")
         .description("Foody 프로젝트 API 명세서입니다.");
 
+    Server server = new Server();
+    server.setUrl("https://map-sv.site");
+
     return new OpenAPI()
         .components(new Components()
             .addSecuritySchemes("bearerAuth", new SecurityScheme()
@@ -28,7 +33,8 @@ public class SwaggerConfig {
                 .scheme("bearer")
                 .bearerFormat("JWT")))
         .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-        .info(info);
+        .info(info)
+        .servers(List.of(server));
   }
 
   @Bean
